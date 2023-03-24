5373bee11d67d92d3226b2d8f0805ceff75e8c33
JJBR2662 <bayernmunich2662@gmail.com>
2023-03-24 10:56:53 -0600

    commit 2

--- src/iniciarsesion_gitbash/Iniciarsesion_gitbash.java
+++ src/iniciarsesion_gitbash/Iniciarsesion_gitbash.java
@@ -15,6 +15,7 @@ public class Iniciarsesion_gitbash {
      */
     public static void main(String[] args) {
         System.out.println("hola");
+        System.out.println("hola");
     }
     
 }
