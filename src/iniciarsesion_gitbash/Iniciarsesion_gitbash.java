56d0763b366f6136ea322f54cbf1660ce1ea7a46
JJBR2662 <bayernmunich2662@gmail.com>
2023-03-24 10:53:29 -0600

    Iniciar sesion

---
+++ src/iniciarsesion_gitbash/Iniciarsesion_gitbash.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package iniciarsesion_gitbash;
+
+/**
+ *
+ * @author roliv
+ */
+public class Iniciarsesion_gitbash {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hola");
+    }
+    
+}
